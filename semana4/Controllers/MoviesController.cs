a04ea94d4b2ee7819f0727562b56dd379f13f8f5
Marcos <mpomeranietz@gmail.com>
2024-04-17 22:28:58 -0300

    Agregar archivos de proyecto.

---
+++ semana4/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace semana4.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //Simulación de creación de un objeto (model)
+            //Mas adelante vamos a ver como usar una base de datos
+            var movie = new Movie
+            {
+                Genero = "Terror",
+                Id = 1,
+                Precio = 1,
+                Fecha_Estreno = DateTime.Now,
+                Titulo = "La noche del terror"
+            };
+
+
+            return View(movie);
+        }
+
+
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var listMovies = new List<Movie>();
+
+            var movie1 = new Movie
+            {
+                Genero = "Terror",
+                Id = 1,
+                Precio = 250,
+                Fecha_Estreno = DateTime.Now,
+                Titulo = "La noche del terror"
+            };
+            listMovies.Add(movie1);
+
+            var movie2 = new Movie
+            {
+                Genero = "Terror",
+                Id = 1,
+                Precio = 300,
+                Fecha_Estreno = DateTime.Now,
+                Titulo = "La noche del terror II"
+            };
+            listMovies.Add(movie2);
+            var movie3 = new Movie
+            {
+                Genero = "Animada",
+                Id = 1,
+                Precio = 350,
+                Fecha_Estreno = DateTime.Now,
+                Titulo = "Sherk"
+            };
+            listMovies.Add(movie3);
+            var movie4 = new Movie
+            {
+                Genero = "Ciencia Ficción",
+                Id = 1,
+                Precio = 500,
+                Fecha_Estreno = DateTime.Now,
+                Titulo = "Dune"
+            };
+            listMovies.Add(movie4);
+
+            return View(listMovies);
+
+        }
+    }
+}
---
+++ semana4/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha_Estreno { get; set; }
+        public string Genero { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
